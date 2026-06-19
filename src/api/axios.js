bc8a0c0c1f677e430db010052ef3e7cdddc16ddb
zayroninfotech <zayroninfotech@gmail.com>
2026-06-19 14:54:02 +0530

    Fix multipart upload: delete Content-Type header for FormData requests so axios sets boundary

--- src/api/axios.js
+++ src/api/axios.js
@@ -8,6 +8,9 @@ const api = axios.create({
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('access_token')
   if (token) config.headers.Authorization = `Bearer ${token}`
+  if (config.data instanceof FormData) {
+    delete config.headers['Content-Type']
+  }
   return config
 })
 
